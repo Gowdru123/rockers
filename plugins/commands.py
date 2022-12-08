f73bf64c680dc4e47347ca57041bee79edb71b06
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2022-12-08 19:01:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('📺 theater print movie's', url='https://t.me/+9NYMiUfQ0RYyOTg1') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('📺theater', url='https://t.me/+9NYMiUfQ0RYyOTg1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
