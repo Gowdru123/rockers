7a4f784309af776f56bdc19f902782991fb992d9
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2022-12-09 12:34:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,11 +435,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('ADD ME TO URGROUP', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('⚡ UPDATES ⚡', url='https://t.me/ROCKERSBACKUP'),
+            InlineKeyboardButton('𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐨𝐟𝐟𝐢𝐜𝐢𝐚𝐥', url='https://t.me/+tU8INwk_RbRlYWM1'),
             InlineKeyboardButton('BOT OWNER ', url='https://t.me/Beastonejnanesh')    
         ], [
-            InlineKeyboardButton('HELP', callback_data='help'),
-            InlineKeyboardButton('ABOUT', callback_data='about')
+            InlineKeyboardButton('🛒 apk games ', url='https://t.me/+AChJcx5l-jc2NWRl'),
+            InlineKeyboardButton('🚨 adult channel', url='https://t.me/fuckofficialforu')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
