08218ddd0fea78cc3f9b038aab788a91b0cae52e
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2022-12-24 22:47:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Gowdru123/url-auto-delet /Eva
+  git clone https://github.com/Gowdru123/rockers /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
