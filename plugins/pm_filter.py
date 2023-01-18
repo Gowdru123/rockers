87968ca324634f11c10686f67686f447ee42683b
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-01-18 16:40:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -158,8 +158,11 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit([
-            InlineKeyboardButton('Click To Check Spelling ✅', f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')])
+            k = await query.message.edit(
+             No Results Found For {event.text} 
+             Type Only Movie Name 
+             newbutton = [Button.url('Click To Check Spelling ✅', f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')],
+                         [Button.url('Click To Check Release Date 📅', f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')]            
             await asyncio.sleep(10)
             await k.delete()
 
